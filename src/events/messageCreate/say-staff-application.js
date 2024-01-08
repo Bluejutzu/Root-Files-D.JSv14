04b6fe9f443055aa7e2ec1d10f9da91e2bde5482
Stonestreet75 <njm.akermann@gmail.com>
2024-01-08 21:08:44 +0100

    Added File

--- src/events/messageCreate/say-staff-application.js
+++ src/events/messageCreate/say-staff-application.js
@@ -1,7 +1,13 @@
-/** @format */
-
 module.exports = (message, client) => {
-    if (message.content === "Where can I apply for staff?") {
-      message.reply("https://discord.com/channels/1124971431093088266/1176944619766951998/1176945293397336199");
-    }
-  };
+  // Convert the message content to lowercase for case-insensitive matching
+  const lowerCaseContent = message.content.toLowerCase();
+
+  // Check if the message contains any of the specified keywords
+  if (
+      lowerCaseContent.includes("how can I join staff") ||
+      lowerCaseContent.includes("how can I become staff")
+  ) {
+    message.reply({content: "You can join the staff team by meeting the requirements found [here](https://discord.com/channels/1124971431093088266/1176944619766951998/1176945293397336199)", ephemeral: true})
+
+  }
+};
