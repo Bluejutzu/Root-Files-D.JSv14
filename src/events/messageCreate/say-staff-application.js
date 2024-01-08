9400deed46aa0ec0e3c8fef1a4fe2b7743a0a9d7
Stonestreet75 <njm.akermann@gmail.com>
2024-01-08 20:10:18 +0100

    Added file

---
+++ src/events/messageCreate/say-staff-application.js
@@ -0,0 +1,7 @@
+/** @format */
+
+module.exports = (message, client) => {
+    if (message.content === "Where can I apply for staff?") {
+      message.reply("https://discord.com/channels/1124971431093088266/1176944619766951998/1176945293397336199");
+    }
+  };
