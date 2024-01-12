285689efaed7a23374d471b6c82d5585ea0afbcc
Bluejutzu <146238506+Bluejutzu@users.noreply.github.com>
2024-01-12 15:57:14 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@
 
 require("dotenv/config");
 const { Client, GatewayIntentBits, Events } = require("discord.js");
-const { CommandKit } = require("commandkit");
-const Bans = require("./models/bans.js");
 const keep_alive = require('./keep_alive.js')
 
 const client = new Client({
@@ -15,17 +13,7 @@ const client = new Client({
   ],
 });
 
-new CommandKit({
-  client,
-  devGuildIds: ["1168532822563233847", "1124971431093088266"],
-  devUserIds: ["368709086008573952", "953708302058012702"],
-  eventsPath: `${__dirname}/events`,
-  commandsPath: `${__dirname}/commands`,
-  validationsPath: `${__dirname}/validations`,
-  bulkRegister: true,
-});
 client.once(Events.ClientReady, (readyClient) => {
-  Bans.sync();
   console.log(`${client.user.username} is ready!`);
 });
 
